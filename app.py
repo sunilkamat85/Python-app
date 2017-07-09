4d823318dbebce594becc1de483b5f1d6261db64
Sunil <sunilkamatec@gmail.com>
2017-07-09 13:30:05 +0530

    these are the end points we will create

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # POST /store data: {name:}
 # GET /store/<string:name>
 # GET /store
-# POST /store/<string:name>/item
+# POST /store/<string:name>/item {name:, price:}
 # GET /store/<string:name>/item
 
 app.run(port=5000)
