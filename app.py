086e209572f59a69274a8317015811278ed400a7
Sunil <sunilkamatec@gmail.com>
2017-07-09 13:27:47 +0530

    these are the end points we will create

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-	return "Hello world!"
-
+# POST /store data: {name:}
+# GET /store/<string:name>
+# GET /store
+# POST /store/<string:name>/item
+# GET /store/<string:name>/item
 
 app.run(port=5000)
